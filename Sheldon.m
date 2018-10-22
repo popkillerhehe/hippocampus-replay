dbe327387cf0663608274a70300e7fd2fa46dbfd
tsangkai <tsangkai.chang@gmail.com>
2018-10-22 16:19:00 -0700

    Add files via upload

---
+++ Sheldon.m
@@ -0,0 +1,44 @@
+% location of files of Sheldon
+
+function [rw_root, vr_root] = Sheldon(date)
+
+if date == 31
+    rw_root = 'Y:\People\Tsang-Kai\RW\Sheldon\2012-07-31_13-46-46';
+    vr_root = 'Y:\People\Tsang-Kai\VR\Sheldon\2012-07-31_15-45-31';
+    
+elseif date == 1
+    rw_root = 'Y:\People\Tsang-Kai\RW\Sheldon\2012-08-01_17-05-57';
+    vr_root = 'Y:\People\Tsang-Kai\VR\Sheldon\2012-08-01_15-16-05';
+    
+%08_06    
+elseif date == 6
+    rw_root = 'Y:\People\Tsang-Kai\RW\Sheldon\2012-08-06_16-29-50';
+    vr_root = 'Y:\People\Tsang-Kai\VR\Sheldon\2012-08-06_14-59-38';
+    
+elseif date == 7
+    rw_root = 'Y:\People\Tsang-Kai\RW\Sheldon\2012-08-07_13-53-14';
+    vr_root = 'Y:\People\Tsang-Kai\VR\Sheldon\2012-08-07_15-51-44';
+    
+elseif date == 13
+    rw_root = 'Y:\People\Tsang-Kai\RW\Sheldon\2012-08-13_17-34-34';
+    vr_root = 'Y:\People\Tsang-Kai\VR\Sheldon\2012-08-13_16-06-22';
+    
+    
+elseif date == 14
+    rw_root = 'Y:\People\Tsang-Kai\RW\Sheldon\2012-08-14_14-36-58';
+    vr_root = 'Y:\People\Tsang-Kai\VR\Sheldon\2012-08-14_16-11-25';
+    
+elseif date == 16
+    rw_root = 'Y:\People\Tsang-Kai\RW\Sheldon\2012-08-16_15-17-17';
+    vr_root = 'Y:\People\Tsang-Kai\VR\Sheldon\2012-08-16_16-59-31';
+    
+elseif date == 20
+    rw_root = 'Y:\People\Tsang-Kai\RW\Sheldon\2012-08-20_17-04-10';
+    vr_root = 'Y:\People\Tsang-Kai\VR\Sheldon\2012-08-20_15-11-21';
+    
+elseif date == 21
+    rw_root = 'Y:\People\Tsang-Kai\RW\Sheldon\2012-08-21_13-15-46';
+    vr_root = 'Y:\People\Tsang-Kai\VR\Sheldon\2012-08-21_14-57-44';
+    
+end
+end
